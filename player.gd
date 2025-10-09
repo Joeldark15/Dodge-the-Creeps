2522d2b69bbeec083580a7dc5eafdcf671101551
Joel <147098517+Joeldark15@users.noreply.github.com>
2025-10-09 15:24:07 +0200

    Creació del node dels enemics.
    Creació del node dels enemics.

--- player.gd
+++ player.gd
@@ -1,9 +1,11 @@
-extends Area2D
+extends Area2D 
+signal hit
 export var speed = 400 # A quina velocitat es mourà el jugador (píxels/seg).
 var screen_size # Mida de la finestra de joc.
 
 func _ready():
 	screen_size = get_viewport_rect().size
+	hide()
 
 func _process(delta):
 	var velocity = Vector2.ZERO # Vector de moviment del jugador.
@@ -23,6 +25,24 @@ func _process(delta):
 	position += velocity * delta
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
+	if velocity.x != 0:
+		$AnimatedSprite.animation = "walk"
+		$AnimatedSprite.flip_v = false
+		$AnimatedSprite.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite.animation = "up"
+		$AnimatedSprite.flip_v = velocity.y > 0
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_player_body_entered(body):
+	hide() # El jugador desapareix després de ser impactat.
+	emit_signal("hit")
+# S'ha d'ajornar, ja que no podem canviar les propietats físiques en una crida de retorn de física.
+	$CollisionShape2D.set_deferred("disabled", true)
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
